54069c78123b2a7f38fc71bf0cc7b2e68d7b953c
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-11-11 23:21:43 -0600

    added navbar element and root color scheme. refactored app.js to hero.js

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,28 +1,35 @@
 import logo from '../../logo.svg';
 import './hero.css';
+import Navbar from '../navbar/navbar.jsx';
 
 function Hero() {
-  return (
-    <div className="hero container">
+    return (
+        <div className="page-wrapper">
+            <Navbar/>
+            <div className="hero container">
 
-      <header className="hero-header row">
-        <div className="hero-images">
-        <img src={logo} className="logo container" alt="logo" />
-      </div>
+                <header className="hero-header row">
+                    <div className="hero-images">
+                        <img src={logo} className="logo container" alt="logo"/>
+                    </div>
 
-      <div className="hero-captions row">
-        <p className="hero-caption">
-          Here are some fun facts about this picture that I am displaying up here. Screenshots or videos of the things I am proud of!
-            Here are some more things about what I am doing to expand my technical skills.
-            Maybe I will display some of the soft skills that I present. I will also have a professional head shot next to the contact form
-            at the bottom of the webpage.
-            I should also work on the other projects that I created during my CodeUp cohort.
-        </p>
-      </div>
+                    <div className="hero-captions row">
+                        <p className="hero-caption">
+                            Here are some fun facts about this picture that I am displaying up here. Screenshots or
+                            videos of the things I am proud of!
+                            Here are some more things about what I am doing to expand my technical skills.
+                            Maybe I will display some of the soft skills that I present. I will also have a professional
+                            head shot next to the contact form
+                            at the bottom of the webpage.
+                            I should also work on the other projects that I created during my CodeUp cohort.
+                        </p>
+                    </div>
 
-      </header>
-    </div>
-  );
+                </header>
+            </div>
+
+        </div>
+    );
 }
 
 export default Hero;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,1 +1,29 @@
 import '../navbar/navbar.css';
+
+
+export default function Navbar() {
+    return (
+        <nav className="navbar container">
+            <div className="navbar-row row">
+                <div className="navbar-logo col">
+                    IMG SRC to banner
+                </div>
+                <div className="portfolio col">
+                    <a>Portfolio</a>
+                </div>
+                <div className="education col">
+                    <a>Education</a>
+                </div>
+                <div className="experience col">
+                    <a>Experience</a>
+                </div>
+                <div className="lang-frame col">
+                    <a>Technical Skills / Frontend / Backend</a>
+                </div>
+                <div className="contact col">
+                    <a>Contact Me!</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
